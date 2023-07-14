6294921b8ec4bdad804f108f6e045828a25ca234
Rodolfo <rodolfo.parada.gonzalez@gmail.com>
2023-07-13 22:07:08 -0400

    prueba

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,5 @@ const {
 } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  // publicPath: process.env.VUE_APP_BASE_URL,
+  publicPath: process.env.VUE_APP_BASE_URL,
 })
